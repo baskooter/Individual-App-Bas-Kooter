11a184df510125925b05a357a1a68d4c7e506490
Bas <bas.kooter@hva.nl>
2020-06-04 12:40:52 +0200

    Trying to fix app on github

---
+++ Workoutapp/app/src/main/java/com/example/workoutapp/StatDao.kt
@@ -0,0 +1,20 @@
+package com.example.workoutapp
+
+import androidx.room.*
+
+@Dao
+interface StatDao {
+
+    @Query("SELECT * FROM statTable")
+    suspend  fun getAllStats(): List<Stats>
+
+    @Insert
+    suspend fun insertStat(stat: Stats)
+
+    @Delete
+    suspend fun deleteStat(stat: Stats)
+
+    @Update
+    suspend fun updatestat(stat: Stats)
+
+}
---
+++ Workoutapp/app/src/main/java/com/example/workoutapp/StatRepository.kt
@@ -0,0 +1,30 @@
+package com.example.workoutapp
+
+import android.content.Context
+
+public class StatRepository(context: Context) {
+
+    private var statDao: StatDao
+
+    init {
+        val statRoomDatabase = StatRoomDatabase.getDatabase(context)
+        statDao = statRoomDatabase!!.statDao()
+    }
+
+    suspend fun getAllStats(): List<Stats> {
+        return statDao.getAllStats()
+    }
+
+    suspend fun insertStat(stat: Stats) {
+        statDao.insertStat(stat)
+    }
+
+    suspend fun deleteStgat(stat: Stats) {
+        statDao.deleteStat(stat)
+    }
+
+    suspend fun updateStat(stat: Stats) {
+        statDao.updatestat(stat)
+    }
+
+}
